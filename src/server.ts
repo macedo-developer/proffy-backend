ee89c7c5b43bb98bd5abe7919a4ee3bd670fdf0d
macedo-developer <macedorenata.developer@gmail.com>
2020-08-04 20:54:21 -0300

    Server example

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello Word");
+import express, { response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (request, response) => {
+  return response.json({ messase: "Hello Word" });
+});
+
+const port = 3002;
+app.listen(port);
