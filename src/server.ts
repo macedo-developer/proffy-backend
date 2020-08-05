3c684aa82be9c7ad66aecd0c9ca94a32079cbf1c
macedo-developer <macedorenata.developer@gmail.com>
2020-08-04 21:43:16 -0300

    Created configurations of database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express, { response } from "express";
+import express, { response } from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/users", (request, response) => {
-  return response.json({ messase: "Hello Word" });
+app.get('/users', (request, response) => {
+  return response.json({ messase: 'Hello Word' });
 });
 
 const port = 3002;
