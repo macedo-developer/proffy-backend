34b42daa883cf2648c73e885e01ea4c5ea5a4a51
macedo-developer <macedorenata.developer@gmail.com>
2020-11-01 22:55:36 -0300

    Altered in controller

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -40,55 +40,47 @@ export default class UsersControllers {
   }
 
   async updateUser(resquest: Request, response: Response) {
-    const {
-      user_id,
-      name,
-      last_name,
-      email,
-      password,
-      bio,
-      avatar,
-      whatsapp,
-      subject,
-      cost,
-      schedule,
-    } = request.body;
-
-    const trx = await db.transaction();
-
-    try {
-      const updateUser = await trx('users')
-        .where('id', user_id)
-        .update({ name, last_name, email, password, bio, avatar, whatsapp });
-
-      const insertedClassesIds = await trx('classes').insert({
-        subject,
-        cost,
-        user_id,
-      });
-
-      const class_id = insertedClassesIds[0];
-
-      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-          class_id,
-          week_day: scheduleItem.week_day,
-          from: convertHourToMinute(scheduleItem.from),
-          to: convertHourToMinute(scheduleItem.to),
-        };
-      });
-
-      await trx('class_schedule').insert(classSchedule);
-
-      await trx.commit();
-
-      return response.status(201).json();
-    } catch (err) {
-      await trx.rollback();
-
-      return response
-        .status(400)
-        .json('Unexpected error while updating profile');
-    }
+    // const {
+    //   name,
+    //   last_name,
+    //   email,
+    //   password,
+    //   bio,
+    //   avatar,
+    //   whatsapp,
+    //   subject,
+    //   cost,
+    //   schedule,
+    // } = request.body;
+    // const { id } = request.params
+    // console.log(id);
+    //   const trx = await db.transaction();
+    //   try {
+    //     const updateUser = await trx('users')
+    //       .where('id', user_id)
+    //       .update({ name, last_name, email, password, bio, avatar, whatsapp });
+    //     const insertedClassesIds = await trx('classes').insert({
+    //       subject,
+    //       cost,
+    //       user_id,
+    //     });
+    //     const class_id = insertedClassesIds[0];
+    //     const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+    //       return {
+    //         class_id,
+    //         week_day: scheduleItem.week_day,
+    //         from: convertHourToMinute(scheduleItem.from),
+    //         to: convertHourToMinute(scheduleItem.to),
+    //       };
+    //     });
+    //     await trx('class_schedule').insert(classSchedule);
+    //     await trx.commit();
+    //     return response.status(201).json();
+    //   } catch (err) {
+    //     await trx.rollback();
+    //     return response
+    //       .status(400)
+    //       .json('Unexpected error while updating profile');
+    //   }
   }
 }
