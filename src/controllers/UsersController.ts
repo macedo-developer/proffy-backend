8798e2a100177c36fe02c93533c24ca29bd3fb08
macedo-developer <macedorenata.developer@gmail.com>
2020-11-09 22:41:28 -0300

    Adjusting method updating user

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,4 +1,4 @@
-import { request, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import db from '../database/connection';
 import convertHourToMinute from '../utils/convertHourToMinute';
 
@@ -39,48 +39,50 @@ export default class UsersControllers {
     return response.json(user);
   }
 
-  async updateUser(resquest: Request, response: Response) {
-    // const {
-    //   name,
-    //   last_name,
-    //   email,
-    //   password,
-    //   bio,
-    //   avatar,
-    //   whatsapp,
-    //   subject,
-    //   cost,
-    //   schedule,
-    // } = request.body;
-    // const { id } = request.params
-    // console.log(id);
-    //   const trx = await db.transaction();
-    //   try {
-    //     const updateUser = await trx('users')
-    //       .where('id', user_id)
-    //       .update({ name, last_name, email, password, bio, avatar, whatsapp });
-    //     const insertedClassesIds = await trx('classes').insert({
-    //       subject,
-    //       cost,
-    //       user_id,
-    //     });
-    //     const class_id = insertedClassesIds[0];
-    //     const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    //       return {
-    //         class_id,
-    //         week_day: scheduleItem.week_day,
-    //         from: convertHourToMinute(scheduleItem.from),
-    //         to: convertHourToMinute(scheduleItem.to),
-    //       };
-    //     });
-    //     await trx('class_schedule').insert(classSchedule);
-    //     await trx.commit();
-    //     return response.status(201).json();
-    //   } catch (err) {
-    //     await trx.rollback();
-    //     return response
-    //       .status(400)
-    //       .json('Unexpected error while updating profile');
-    //   }
+  async updateUser(request: Request, response: Response) {
+    const {
+      name,
+      last_name,
+      email,
+      password,
+      bio,
+      avatar,
+      whatsapp,
+      subject,
+      cost,
+      schedule,
+    } = request.body;
+
+    const { id } = request.params;
+
+    const trx = await db.transaction();
+    try {
+      const updateUser = await trx('users')
+        .where('id', id)
+        .update({ name, last_name, email, password, bio, avatar, whatsapp });
+
+      const insertedClassesIds = await trx('classes').insert({
+        subject,
+        cost,
+        user_id: id,
+      });
+      const class_id = insertedClassesIds[0];
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinute(scheduleItem.from),
+          to: convertHourToMinute(scheduleItem.to),
+        };
+      });
+      await trx('class_schedule').insert(classSchedule);
+      await trx.commit();
+      return response.status(201).json();
+    } catch (err) {
+      await trx.rollback();
+      return response
+        .status(400)
+        .json('Unexpected error while updating profile');
+    }
   }
 }
