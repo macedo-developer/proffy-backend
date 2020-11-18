f20e3cb645056f1cc2170deefff002d89b4c953a
macedo-developer <macedorenata.developer@gmail.com>
2020-11-17 23:50:23 -0300

    Editing controller

--- src/controllers/ClassesScheduleController.ts
+++ src/controllers/ClassesScheduleController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
+import db from '../database/connection';
 
 export default class ClassesScheduleController {
   async show(request: Request, response: Response) {
     const { class_id } = request.params;
+
+    const schedules = await db('class_schedule')
+      .where('class_id', '=', class_id)
+      .select('*');
+
+    return response.json(schedules);
   }
 }
