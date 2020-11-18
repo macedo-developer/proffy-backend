9100524b9ec674487bd67906b2d19a11424436cf
macedo-developer <macedorenata.developer@gmail.com>
2020-11-17 23:49:20 -0300

    Add in routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ClassesScheduleController from './controllers/ClassesScheduleController';
 import ConnectionsController from './controllers/ConnectionsController';
 import UsersControllers from './controllers/UsersController';
 
 const routes = express.Router();
 
+const classesScheduleController = new ClassesScheduleController();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
 const usersControllers = new UsersControllers();
@@ -17,6 +19,8 @@ routes.put('/users/:id', usersControllers.updateUser);
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
+
+routes.get('/schedule/:class_id', classesScheduleController.show);
 
 routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
